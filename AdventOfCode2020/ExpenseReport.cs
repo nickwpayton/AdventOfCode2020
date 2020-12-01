e1f811bc98b671d6654a534c22ebbdf4e9529051
Cole Payton <ucpayto@arcb.com>
2020-12-01 09:08:15 -0600

    Day 1 Part 1

---
+++ AdventOfCode2020/ExpenseReport.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020
+{
+    public class ExpenseReport
+    {
+        public Tuple<int,int> FindTwoEntriesSummingToTotal(int total, List<int> entries)
+        {
+            foreach(var entry in entries)
+            {
+                int? sumCandidate = entries.Where(e => (total - e) == entry).Cast<int?>().FirstOrDefault();
+                if (sumCandidate != null)
+                {
+                    return Tuple.Create(entry, sumCandidate.Value);
+                }
+            }
+
+            return Tuple.Create(0, 0);
+        }
+
+        public List<int> FindThreeEntriesSummingToTotal(int total, List<int> entries)
+        {
+            foreach (var entry in entries)
+            {
+                var remainingEntries = entries.RemoveFirst();
+
+                foreach (var remainingEntry in remainingEntries)
+                {
+                    int? sumCandidate = remainingEntries.Where(re => (total - re - remainingEntry) == entry).Cast<int?>().FirstOrDefault();
+                    if (sumCandidate != null)
+                    {
+                        return new List<int> { entry, remainingEntry, sumCandidate.Value };
+                    }
+                }
+            }
+
+            return new List<int>();
+        }
+    }
+}
---
+++ AdventOfCode2020/LinqHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode2020
+{
+    public static class LinqHelper
+    {
+        public static List<int> RemoveFirst(this List<int> list)
+        {
+            list.RemoveAt(0);
+
+            return list;
+        }
+
+        public static int MultiSum(this List<int> list)
+        {
+            var total = 1;
+
+            foreach(var item in list)
+            {
+                total = total * item;
+            }
+
+            return total;
+        }
+    }
+}
