d210a6306ec2143438ff89b21e21e72d4de61ebd
Cole Payton <ucpayto@arcb.com>
2020-12-04 07:00:27 -0600

    no idea why it's still not working

--- AdventOfCode2020/FileReader.cs
+++ AdventOfCode2020/FileReader.cs
@@ -33,11 +33,11 @@ namespace AdventOfCode2020
             return stringLists;
         }
 
-        public List<string> GetLinesFromFile(string fileExtension)
+        public string[] GetLinesFromFile(string fileExtension)
         {
             string[] lines = System.IO.File.ReadAllLines(fileExtension);
 
-            return lines.ToList();
+            return lines;
         }
     }
 }
--- AdventOfCode2020/IntegrationTests/Day3.cs
+++ AdventOfCode2020/IntegrationTests/Day3.cs
@@ -8,31 +8,64 @@ namespace AdventOfCode2020.IntegrationTests.Day1
     [TestFixture]
     class Day3
     {
+        private int _treeEncounters;
+
         [Test]
         public void Part1_GetTreeEncounterCount()
         {
             var fileExtension = @"C:\Users\ucpayto\source\repos\AdventOfCode2020\day-3-input.txt";
             var fileReader = new FileReader();
-            var treeGrid = fileReader.GetLinesFromFile(fileExtension);
-            var patternRepeatsAt = treeGrid.First().Length;
-            var forest = string.Empty;
+            var treeGrid = fileReader.GetLinesFromFile(fileExtension).ToList();
 
-            foreach (var line in treeGrid) 
-            {
-                forest += line;
-            }
+            //char[][] forest = treeGrid.Select(tile => tile.ToArray()).ToArray();
+            var down = 2;
+            var right = 1;
 
-            var iteration = patternRepeatsAt + 3;
-            var treeEncounters = 0; 
-            for (int i = 0; i < forest.Length; i += iteration)
+            var result = new List<string>();
+
+            for (int x = 0; x < treeGrid.Count; x++)
             {
-                if (forest[i] == '#')
+                var rowOfTrees = string.Empty;
+
+                for (int y = 0; y < treeGrid.Count * right; y += treeGrid[x].Length)
                 {
-                    treeEncounters++;
+                    rowOfTrees += treeGrid[x];
                 }
+
+                result.Add(rowOfTrees);
             }
 
+            char[][] forest = result.Select(tile => tile.ToArray()).ToArray();
+
+            int i = 1;
+            int j = 0;
+            var forestRowLength = forest[i].Length;
+            while (i < forest.Length)
+            {
+                //if (j + right > forestRowLength - 1)
+                //{
+                //    j = j + right - forestRowLength;
+                //    TallyTreeEncounter(forest[i][j]);
+                //}
+                //else
+                //{
+                //    j += right;
+                //    TallyTreeEncounter(forest[i][j]);
+                //}
+                j += right;
+                TallyTreeEncounter(forest[i][j]);
+                i += down;
+            }
+            var encounters = _treeEncounters;
             var twentyTwenty = 2020;
+        }
+
+        private void TallyTreeEncounter(char tile)
+        {
+            if(tile == '#')
+            {
+                _treeEncounters++;
+            }
         }
     }
 }
